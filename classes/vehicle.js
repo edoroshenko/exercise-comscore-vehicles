fc16844972f4ed8b085cccd12b1f3871e1d3e582
Evgeniy Doroshenko <esdoroshenko@esdoroshenko-osx.local>
2012-11-10 18:34:33 +0400

    events engine

--- classes/vehicle.js
+++ classes/vehicle.js
@@ -1,12 +1,27 @@
 var Vehicle = function(options) {
 
+	this.consumtionFuel = options.consumtionFuel;
+	this.consumtionOil  = options.consumtionOil;
+	this.nameUnit       = options.nameUnit;
+	// How much units does the vehice carry per 100 km
+	this.volumeUnits    = options.volumeUnits;
+	// The speed of the vehicle in km per hour
+	this.speed = 60;
+	// shows how often to change the state of the instance
+	this.timeoutStep = 10;
 };
 
-Vehicle.prototype.startMoving = function() {
+Vehicle.prototype.resetCounters = function() {
+	this.usageFuel     = 0;
+	this.usageOil      = 0;
+	this.carriedUnits  = 0;
+};
+
+Vehicle.prototype.run = function(distance) {
 
 };
 
-Vehicle.prototype.stopMoving = function() {
+Vehicle.prototype.stop = function() {
 	
 };
 
