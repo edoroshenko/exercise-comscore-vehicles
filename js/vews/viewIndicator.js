5cd2ccc3dc2faa72b3b5ff9ee2f02ce9567c080d
Evgeniy Doroshenko <esdoroshenko@esdoroshenko-osx.local>
2012-11-11 21:54:47 +0400

    view objects

---
+++ js/vews/viewIndicator.js
@@ -0,0 +1,18 @@
+var ViewIndicator = function(node) {
+	this.node = node;
+};
+
+ViewIndicator.prototype.bindAll = function() {
+
+};
+
+ViewIndicator.prototype.reset = function() {
+	// width of progressbar - a distance
+	// fuel
+	// oil
+	// load (if specified)
+};
+
+ViewIndicator.prototype.update = function() {
+	
+};
